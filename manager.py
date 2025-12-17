77700eca9253e6a104fcbd3f1febda2a4c39ad89
Aymane Baabi <bbm3842a@u6-100-20d.univ-tlse3.fr>
2025-12-17 16:53:12 +0100

    TP-4: interopérabilité C++/Python via proxy HTTP
    ## Proxy (Python)
    - Serveur HTTP bas niveau sur port 8000
    - Connexion au manager TP-2 via QueueClient
    - Utilisation de Task.to_json()/from_json()
    
    ## Client C++
    - Utilisation de CPR pour requêtes HTTP
    - Parsing JSON avec nlohmann/json
    - Intégration Eigen (préparée)
    - CMakeLists.txt avec FetchContent
    
    ## Fonctionnalités
    - GET / : Récupère une Task depuis la file
    - POST / : Envoie une Task vers la file résultats
    - Communication bidirectionnelle C++ ↔ Python

---
+++ manager.py
@@ -0,0 +1,19 @@
+import multiprocessing as mp
+from multiprocessing.managers import BaseManager
+
+class QueueManager(BaseManager):
+    pass
+
+class QueueClient:
+    def __init__(self):
+        QueueManager.register("get_task_queue")
+        QueueManager.register("get_result_queue")
+
+        manager = QueueManager(address=("localhost", 50000), authkey=b"secret")
+        manager.connect()
+
+        self.task_queue = manager.get_task_queue()
+        self.result_queue = manager.get_result_queue()
+
+if __name__ == "__main__":
+    print("QueueClient module pour proxy.py")
--- minion.py
+++ minion.py
@@ -1,13 +1,1 @@
-def worker(task_queue, result_queue):
-    while True:
-        task = task_queue.get()
-
-        if task is None:
-            break
-
-        task.work()
-        result_queue.put((task.identifier, task.time))
-
-
-if __name__ == "__main__":
-    print("Module minion.")
+404: Not Found
