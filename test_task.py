41f02e7379e51636dff31a2553a4e83c4620ed9a
Aymane Baabi <bbm3842a@u6-100-20d.univ-tlse3.fr>
2025-12-17 14:08:43 +0100

    fix: correction test_task.py (work() -> solve())

--- test_task.py
+++ test_task.py
@@ -6,9 +6,8 @@ from task import Task
 class TestTask(unittest.TestCase):
     def test_solution(self):
         task = Task()
-        task.work()  # Résoudre Ax = B
-        Ax = task.a @ task.x
-        np.testing.assert_allclose(Ax, task.b, rtol=1e-7, atol=0)
+        A, B, x = task.solve()
+        np.testing.assert_allclose(A @ x, B, rtol=1e-7, atol=0)
 
 
 if __name__ == "__main__":
